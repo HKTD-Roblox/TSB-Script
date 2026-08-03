2349a6694cb1d461a447b7f2ad2a81030c9544cf
Hoàng Kim Tiến Đạt <thuykieu071980@gmail.com>
2026-08-03 21:26:35 +0700

    Update Ultra-Instinct.lua
    Updated loadstring calls to enable secure execution.

--- Ultra-Instinct.lua
+++ Ultra-Instinct.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/HKTD-Roblox/Script-Beta/refs/heads/main/Script-By-HKTD.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/IdkRandomUsernameok/PublicAssets/refs/heads/main/Releases/MUI.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HKTD-Roblox/Script-Beta/refs/heads/main/Script-By-HKTD.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/IdkRandomUsernameok/PublicAssets/refs/heads/main/Releases/MUI.lua", true))()
